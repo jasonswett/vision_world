cfff2ab0927d1b5f307319aa23e094f9c87ee92a
Jason Swett <jason@benfranklinlabs.com>
2023-07-27 10:11:09 -0400

    Remove screen.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from move import Move
 from cell_screen import CellScreen
 from cell import Cell
 from organism import Organism
-from small_screen import SmallScreen
 
 def main():
     SCREEN_WIDTH_IN_CELLS = 100
@@ -33,12 +32,6 @@ def main():
             cell_screen.draw_cell(cell)
 
         cell_screen.draw_organism(johnny)
-        small_screen = SmallScreen(cell_screen.surface, cell_screen.width_in_pixels(), cell_screen.height_in_pixels())
-        small_screen.draw()
-        small_screen.draw_indicator(johnny.cells[0].north_color(food_cells), 1, 0)
-        small_screen.draw_indicator(johnny.cells[0].south_color(food_cells), 1, 2)
-        small_screen.draw_indicator(johnny.cells[0].east_color(food_cells), 2, 1)
-        small_screen.draw_indicator(johnny.cells[0].west_color(food_cells), 0, 1)
 
         pygame.display.update()
         clock.tick(60)
