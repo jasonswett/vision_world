0260949eb8322d76a0482933b6c926c841f94e87
Jason Swett <jason@benfranklinlabs.com>
2023-07-27 21:50:31 -0400

    Refactoring.

--- cell_search.py
+++ cell_search.py
@@ -1,18 +1,18 @@
 class CellSearch:
-    def __init__(self, cells, fixed_coord, start, direction, max_distance, is_vertical):
+    def __init__(self, cells, origin_x, origin_y, direction, max_distance, is_vertical):
         self.cells = cells
-        self.fixed_coord = fixed_coord
-        self.start = start
+        self.origin_x = origin_x
+        self.origin_y = origin_y
         self.direction = direction
         self.max_distance = max_distance
         self.is_vertical = is_vertical
 
     def nearest_cell(self):
-        for i in range(self.start, self.start + self.direction * self.max_distance, self.direction):
+        for i in range(self.origin_y, self.origin_y + self.direction * self.max_distance, self.direction):
             for cell in self.cells:
                 if self.is_match(cell, i):
                     return cell
         return None
 
     def is_match(self, cell, i):
-        return (cell.y == i and cell.x == self.fixed_coord) if self.is_vertical else (cell.x == i and cell.y == self.fixed_coord)
+        return (cell.y == i and cell.x == self.origin_x) if self.is_vertical else (cell.x == i and cell.y == self.origin_x)
