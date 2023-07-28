830ddcd93650c0d7aa1156bf31d0b78190478086
Jason Swett <jason@benfranklinlabs.com>
2023-07-27 22:44:25 -0400

    Refactoring.

--- eye_cell.py
+++ eye_cell.py
@@ -3,12 +3,6 @@ from cell_search import CellSearch
 
 class EyeCell(Cell):
     def digest(self, food_cells):
-        total = 0
-        findings = [
-            CellSearch(food_cells, self.x, self.y, 0, -1).nearest_cell(),
-            CellSearch(food_cells, self.x, self.y, 0, 1).nearest_cell(),
-            CellSearch(food_cells, self.x, self.y, -1, 0).nearest_cell(),
-            CellSearch(food_cells, self.x, self.y, 1, 0).nearest_cell()
-        ]
-        binary_string = "".join(["1" if finding else "0" for finding in findings])
-        return binary_string
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        findings = [CellSearch(food_cells, self.x, self.y, dx, dy).nearest_cell() for dx, dy in directions]
+        return "".join(["1" if finding else "0" for finding in findings])
