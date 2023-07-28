5d62aa37b226fb2d9b0d8763c1bd1c81d49c4a52
Jason Swett <jason@benfranklinlabs.com>
2023-07-27 21:47:40 -0400

    Refactoring.

--- eye_cell.py
+++ eye_cell.py
@@ -4,13 +4,13 @@ from cell_search import CellSearch
 MAX_DISTANCE_TO_LOOK = 10
 
 class EyeCell(Cell):
-    def digest(self, cells):
+    def digest(self, food_cells):
         total = 0
         checks = [
-            CellSearch(cells, self.x, self.y, -1, MAX_DISTANCE_TO_LOOK, True).nearest_cell(),
-            CellSearch(cells, self.y, self.x, 1, MAX_DISTANCE_TO_LOOK, False).nearest_cell(),
-            CellSearch(cells, self.x, self.y, 1, MAX_DISTANCE_TO_LOOK, True).nearest_cell(),
-            CellSearch(cells, self.y, self.x, -1, MAX_DISTANCE_TO_LOOK, False).nearest_cell()
+            CellSearch(food_cells, self.x, self.y, -1, MAX_DISTANCE_TO_LOOK, True).nearest_cell(),
+            CellSearch(food_cells, self.y, self.x, 1, MAX_DISTANCE_TO_LOOK, False).nearest_cell(),
+            CellSearch(food_cells, self.x, self.y, 1, MAX_DISTANCE_TO_LOOK, True).nearest_cell(),
+            CellSearch(food_cells, self.y, self.x, -1, MAX_DISTANCE_TO_LOOK, False).nearest_cell()
         ]
         binary_string = "".join(["1" if check else "0" for check in checks])
         return int(binary_string, 2)
