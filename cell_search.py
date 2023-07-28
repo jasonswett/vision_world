3a454e41dcbeeb2f0e865673140efc1155410064
Jason Swett <jason@benfranklinlabs.com>
2023-07-27 22:01:29 -0400

    Refactoring.

--- cell_search.py
+++ cell_search.py
@@ -1,19 +1,23 @@
 MAX_DISTANCE_TO_LOOK = 10
 
 class CellSearch:
-    def __init__(self, cells, origin_x, origin_y, direction, is_vertical):
+    def __init__(self, cells, origin_x, origin_y, x_direction, y_direction):
         self.cells = cells
         self.origin_x = origin_x
         self.origin_y = origin_y
-        self.direction = direction
-        self.is_vertical = is_vertical
+        self.x_direction = x_direction
+        self.y_direction = y_direction
 
     def nearest_cell(self):
-        for i in range(self.origin_y, self.origin_y + self.direction * MAX_DISTANCE_TO_LOOK, self.direction):
-            for cell in self.cells:
-                if self.is_match(cell, i):
-                    return cell
+        x_range = range(self.origin_x, self.origin_x + self.x_direction * MAX_DISTANCE_TO_LOOK, self.x_direction or 1)
+        y_range = range(self.origin_y, self.origin_y + self.y_direction * MAX_DISTANCE_TO_LOOK, self.y_direction or 1)
+
+        for i in x_range:
+            for j in y_range:
+                for cell in self.cells:
+                    if self.is_match(cell, i, j):
+                        return cell
         return None
 
-    def is_match(self, cell, i):
-        return (cell.y == i and cell.x == self.origin_x) if self.is_vertical else (cell.x == i and cell.y == self.origin_x)
+    def is_match(self, cell, i, j):
+        return cell.x == i and cell.y == j
--- eye_cell.py
+++ eye_cell.py
@@ -5,10 +5,10 @@ class EyeCell(Cell):
     def digest(self, food_cells):
         total = 0
         checks = [
-            CellSearch(food_cells, self.x, self.y, -1, True).nearest_cell(),
-            CellSearch(food_cells, self.y, self.x, 1, False).nearest_cell(),
-            CellSearch(food_cells, self.x, self.y, 1, True).nearest_cell(),
-            CellSearch(food_cells, self.y, self.x, -1, False).nearest_cell()
+            CellSearch(food_cells, self.x, self.y, 0, -1).nearest_cell(),
+            CellSearch(food_cells, self.y, self.x, 0, 1).nearest_cell(),
+            CellSearch(food_cells, self.x, self.y, -1, 0).nearest_cell(),
+            CellSearch(food_cells, self.y, self.x, 1, 0).nearest_cell()
         ]
         binary_string = "".join(["1" if check else "0" for check in checks])
         return int(binary_string, 2)
