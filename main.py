bb31b5696849fae9a75e95572d83793c17ab44eb
Jason Swett <jason@benfranklinlabs.com>
2023-08-03 22:57:37 -0400

    Make it so each generation starts fresh.

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ def main():
 
         if is_population_at_reproduction_threshold(ecosystem.organisms):
             healthiest_organisms = organisms_ordered_by_health(ecosystem.organisms)[:NUMBER_OF_ORGANISMS_ALLOWED_TO_REPRODUCE]
+            ecosystem.organisms = []
             ecosystem.organisms.extend(Generation(healthiest_organisms, cell_screen).offspring())
             ecosystem.food_cells.clear()
             ecosystem.food_cells.extend(ecosystem.starting_food_cells())
